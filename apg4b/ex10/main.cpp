fd62f3eed2ef87ed0eeed4f5c8d782da16a7603b
kengoMBP <kengo0805ooo@gmail.com>
2025-07-13 18:03:29 +0900

    add:ex10

---
+++ apg4b/ex10/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+  // ここにプログラムを追記
+    cout << "A:";
+    while(A  > 0){
+        cout << "]";
+        A--;
+    } 
+    cout << endl;
+    cout << "B:";
+    while(B > 0){
+        cout << "]";
+        B--;
+    } 
+    cout << endl;
+    
+    return 0;
+}
