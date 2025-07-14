3afdb299b4cfa0f7769f1e791f2c1efeabb52ce3
kengoMBP <kengo0805ooo@gmail.com>
2025-07-14 23:11:01 +0900

    add:ex11

---
+++ apg4b/ex11/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+#define ll long long
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+    int N, A;
+    cin >> N >> A;
+    for(int i = 0; i < N; i++){
+        string op;
+        int B;
+        cin >> op >> B;
+        if(op == "+"){
+            A += B;
+        }
+        else if(op == "-"){
+            A -= B;
+        }
+        else if(op == "*"){
+            A *= B;
+        }
+        else if(op == "/"){
+            if(B == 0){
+                cout << "error" << endl;
+                break;
+            }
+            A /= B;
+        }
+        cout << i + 1 << ":" << A << endl;
+    }
+        
+    
+    
+    return 0;
+}
