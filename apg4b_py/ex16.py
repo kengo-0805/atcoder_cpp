512e33928b31b76868b2b7c2545146cd87227fd8
kengoMBP <kengo0805ooo@gmail.com>
2025-08-05 00:10:43 +0900

    fix:python ex16(3回目)

--- apg4b_py/ex16.py
+++ apg4b_py/ex16.py
@@ -1,7 +1,6 @@
 n, k = map(int, input().split())
-words = input().split()
-new_words = []
-for i in range(n):
-    if len(words[i]) >= k:
-        new_words.append(words[i])
-print(*new_words)
+s = input().split()
+
+ans = [word for word in s if len(word) >= k]
+
+print(*ans)
